5fff66d1f9dda463bb41eb2e34e85be7f920ee39
yb <lojobs38@gmail.com>
2023-05-17 13:58:36 +0800

    feat: use map to handle multi user

--- src/main/java/org/example/WebVitalsServer.java
+++ src/main/java/org/example/WebVitalsServer.java
@@ -5,26 +5,39 @@ import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Path("/metrics")
 public class WebVitalsServer {
-    private List<Object> webVitalsResult = new ArrayList<>();
-
+    Map<String, List<Object>> mapObj = new HashMap<>();
 
     @GET
+    public Response sayHi() {
+        Object[] keySetMap = mapObj.keySet().toArray();
+        return Response.ok(keySetMap).build();
+    }
+
+    @GET
+    @Path("/{id}")
     @Produces(MediaType.APPLICATION_JSON)
-    public Response getWV() {
-        return Response.ok(webVitalsResult).build();
+    public Response getWVWithId(String id) {
+        return Response.ok(mapObj.get(id)).build();
     }
 
     @POST
+    @Path("/{id}")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public Response receiveWV(final Object dataReceive) {
-//        System.out.println(request.headers().toString());
+    public Response receiveWVWithId(@PathParam("id") String id, final Object dataReceive) {
 //        System.out.println(dataReceive.toString());
-        webVitalsResult.add(dataReceive);
+        List<Object> webVitalsResultList = mapObj.get(id);
+        if (webVitalsResultList == null) {
+            webVitalsResultList = new ArrayList<>();
+        }
+        webVitalsResultList.add(dataReceive);
+        mapObj.put(id, webVitalsResultList);
         return Response.ok("{\"status\": \"success\"}").build();
     }
 }
